e0d020653bdd8598f32195ea4702b792a3b5585b
DvanKooten <DvanKooten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-09 11:34:20 +0000

    Version 1.0.6 - getting ready for WP 3.8 + bugfixes + various improvements.
    git-svn-id: https://plugins.svn.wordpress.org/scroll-triggered-boxes/trunk@817350 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/views/metabox-dvk-support.php
@@ -0,0 +1,8 @@
+<?php 
+if ( ! defined( 'ABSPATH' ) ) {
+	header( 'HTTP/1.0 403 Forbidden' );
+	header( 'X-Robots-Tag: noindex' );
+	exit;
+} ?>
+<p>Please use the <a href="http://wordpress.org/support/plugin/scroll-triggered-boxes/">plugin support forums</a> on WordPress.org.</p>
+
