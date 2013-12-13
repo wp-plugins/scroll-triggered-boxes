0d6389a996347602c485bb1781848155ca624e54
DvanKooten <DvanKooten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-13 10:23:53 +0000

    Version 1.1 - added test mode
    git-svn-id: https://plugins.svn.wordpress.org/scroll-triggered-boxes/tags/1.1@820931 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/index.php
@@ -0,0 +1,7 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	header( 'HTTP/1.0 403 Forbidden' );
+	header( 'X-Robots-Tag: noindex' );
+	exit;
+}
---
+++ includes/views/metabox-dvk-donate.php
@@ -0,0 +1,17 @@
+<?php 
+if ( ! defined( 'ABSPATH' ) ) {
+	header( 'HTTP/1.0 403 Forbidden' );
+	header( 'X-Robots-Tag: noindex' );
+	exit;
+} ?>
+<p>If you like this plugin, consider supporting it by donating a token of your appreciation.</p>
+
+<a href="http://dannyvankooten.com/donate/" class="button-primary">Donate with PayPal</a>
+
+<p>Some other ways to support this plugin</p>
+<ul class="ul-square">
+	<li><a href="http://wordpress.org/support/view/plugin-reviews/scroll-triggered-boxes?rate=5#postform" target="_blank">Leave a &#9733;&#9733;&#9733;&#9733;&#9733; review on WordPress.org</a></li>
+	<li><a href="http://dannyvankooten.com/wordpress-plugins/scroll-triggered-boxes/" target="_blank">Link to the plugin page from your blog</a></li>
+	<li><a href="http://twitter.com/intent/tweet/?text=<?php echo urlencode('I am using Scroll Triggered Boxes on my WordPress site. It\'s great!'); ?>&via=DannyvanKooten&url=<?php echo urlencode('http://wordpress.org/plugins/scroll-triggered-boxes/'); ?>" target="_blank">Tweet about Scroll Triggered Boxes</a></li>
+	<li><a href="http://wordpress.org/plugins/scroll-triggered-boxes/#compatibility">Vote "works" on the WordPress.org plugin page</a></li>
+</ul>
