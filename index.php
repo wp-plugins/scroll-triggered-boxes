4979278f2d962ce3e13bcea16ca6ebcf470d5ac9
DvanKooten <DvanKooten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-08 19:22:54 +0000

    fixed some cookie issues
    git-svn-id: https://plugins.svn.wordpress.org/scroll-triggered-boxes/tags/1.1.7@835098 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Scroll Triggered Boxes
-Version: 1.1.5
+Version: 1.1.7
 Plugin URI: http://dannyvankooten.com/
 Description: Call-To-Action Boxes that display after visitors scroll down far enough. Highly conversing, not so annoying!
 Author: Danny van Kooten
@@ -33,7 +33,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define("STB_VERSION", "1.1.5");
+define("STB_VERSION", "1.1.7");
 define("STB_PLUGIN_DIR", plugin_dir_path(__FILE__)); 
 define("STB_PLUGIN_URL", plugins_url( '/' , __FILE__ ));
 
